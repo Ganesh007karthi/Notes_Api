4b2595303edaa14258cb8cdef50d2f867cef1ed5
ganeshkarthi007 <ganeshsmarty98@gmail.com>
2019-10-30 20:44:33 +0530

    final commit

--- src/main/java/com/example/notes/Dao/TaskDao.java
+++ src/main/java/com/example/notes/Dao/TaskDao.java
@@ -1,9 +1,9 @@
 package com.example.notes.Dao;
-
 import com.example.notes.model.Notes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskDao extends JpaRepository<Notes,Long> {
+
 }
